8034ab22668ad2c60ae8542d46c741d0b72cc3f4
Michał Zaręba <michalzareba0001@gmail.com>
2022-08-27 13:50:59 +0200

    ball movement

---
+++ script.js
@@ -0,0 +1,53 @@
+var gameboard = document.getElementById('gameboard');
+var floor = document.getElementById("floor");
+var floorBottom = parseInt(window.getComputedStyle(floor).getPropertyValue("bottom"));
+var floorHeight = parseInt(window.getComputedStyle(floor).getPropertyValue("height"));
+var ball = document.getElementById("ball");
+var ballBottom = parseInt(window.getComputedStyle(ball).getPropertyValue("bottom"));
+var ballHeight = parseInt(window.getComputedStyle(ball).getPropertyValue("height"));
+var ballRight = parseInt(window.getComputedStyle(ball).getPropertyValue("right"));
+var ballWidth = parseInt(window.getComputedStyle(ball).getPropertyValue("width"));
+var isJumping = false;
+var upTime;
+var downTime;
+console.log(ballRight);
+function jump() {
+    if (isJumping)
+        return;
+    upTime = setInterval(function () {
+        if (ballBottom >= floorHeight + 250) {
+            clearInterval(upTime);
+            downTime = setInterval(function () {
+                if (ballBottom <= floorHeight + 10) {
+                    clearInterval(downTime);
+                    isJumping = false;
+                }
+                ballBottom -= 10;
+                ball.style.bottom = ballBottom + "px";
+            }, 20);
+        }
+        ballBottom += 10;
+        ball.style.bottom = ballBottom + "px";
+        isJumping = true;
+    }, 20);
+}
+function moveBallRight() {
+    ballRight -= 10;
+    ball.style.right = ballRight + "px";
+}
+function moveBallLeft() {
+    ballRight += 10;
+    ball.style.right = ballRight + "px";
+}
+function control(e) {
+    if (e.key == "ArrowUp" || e.key == " ") {
+        jump();
+    }
+    if (e.key == "ArrowRight") {
+        moveBallRight();
+    }
+    if (e.key == "ArrowLeft") {
+        moveBallLeft();
+    }
+}
+document.addEventListener("keydown", control);
